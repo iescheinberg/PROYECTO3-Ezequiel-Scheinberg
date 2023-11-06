a209a24bdc59a573c309ff613e911f62b59b5161
iescheinberg <agr.scheinberg@gmail.com>
2023-11-06 14:30:51 -0300

    components+css+errors

---
+++ src/Pages/Historial.jsx
@@ -0,0 +1,11 @@
+import { Busqueda } from "../Components/Busqueda";
+
+export function Historial() {
+    return (
+        <>
+            <Busqueda />
+        </>
+    );
+}
+
+export default Historial;
---
+++ src/Pages/Principal.jsx
@@ -0,0 +1,13 @@
+import Encabezado from "../Components/Encabezado";
+import Formulario from "../Components/Formulario";
+
+export function Principal() {
+    return (
+        <>
+            <Encabezado />
+            <Formulario />
+        </>
+    );
+}
+
+export default Principal;
